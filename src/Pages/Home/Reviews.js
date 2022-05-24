8371c94150edbd90cc0f55ffae804e35a8887650
devnoman01 <devnoman01@gmail.com>
2022-05-24 13:02:19 +0600

    react rating and fontawesome icon implement complete

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -23,9 +23,9 @@ const Reviews = () => {
           Users Reviews
         </h1>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {/* {reviews.map((review) => (
+          {reviews.map((review) => (
             <ReviewCard key={review._id} review={review} />
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
